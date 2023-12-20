933114849054a24b552342844b85888f52b3c8b8
Taki Eddine Yamani <alikondie@gmail.com>
2023-12-20 22:23:28 +0100

    day1

---
+++ day1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// 53254
+func main() {
+	var digits = map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	var firstDigit string
+	var secondDigit string
+	totalCount := 0
+	startIndex := 0
+	line := 3
+	for index, char := range input {
+		for key, digit := range digits {
+			if strings.Contains(input[startIndex:index], key) {
+				fmt.Println(input[startIndex:index])
+				if firstDigit == "" {
+					firstDigit = fmt.Sprint(digit)
+				}
+				secondDigit = fmt.Sprint(digit)
+				startIndex = index - 1
+			}
+		}
+
+		if unicode.IsDigit(char) {
+			if firstDigit == "" {
+				firstDigit = string(char)
+			}
+			secondDigit = string(char)
+		}
+		if char == '\n' {
+			number, _ := strconv.Atoi(firstDigit + secondDigit)
+			fmt.Println(number)
+			line++
+			totalCount += number
+			firstDigit = ""
+			secondDigit = ""
+			startIndex = index - 1
+		}
+	}
+	fmt.Println(totalCount)
+}
